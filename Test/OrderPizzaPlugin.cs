f4f73ca305afbe24f29d898b91e36a419786697c
zihao <wzihao@unimaxtech.com.tw>
2025-02-05 17:01:45 +0800

    加入專案檔案。

---
+++ Test/OrderPizzaPlugin.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace Test
+{
+    public class OrderPizzaPlugin
+    {
+        public Cart _Cart = new Cart();
+        public Menu _Menu = new Menu();
+
+        [KernelFunction("get_pizza_menu")]
+        public Menu GetPizzaMenuAsync()
+        {
+            return _Menu;
+        }
+
+        [KernelFunction("add_pizza_to_cart")]
+        [Description("Add a pizza to the user's cart; returns the new item and updated cart")]
+        public void AddPizzaToCart(
+           int pizzaId
+        )
+        {
+            var pizza = _Menu.pizzas.FirstOrDefault(x => x.Id == pizzaId);
+
+            if (pizza == null)
+                return;
+
+            _Cart.pizzas.Add(pizza);
+        }
+
+        [KernelFunction("remove_pizza_from_cart")]
+        public void RemovePizzaFromCart(int pizzaId)
+        {
+            var pizza = _Cart.pizzas.FirstOrDefault(x => x.Id == pizzaId);
+
+            if (pizza == null)
+                return;
+
+            _Cart.pizzas.Remove(pizza);
+        }
+
+        [KernelFunction("get_pizza_from_cart")]
+        [Description("Returns the specific details of a pizza in the user's cart; use this instead of relying on previous messages since the cart may have changed since then.")]
+        public Pizza? GetPizzaFromCart(int pizzaId)
+        {
+            return _Cart.pizzas.FirstOrDefault(x => x.Id == pizzaId);
+        }
+
+        [KernelFunction("get_cart")]
+        [Description("Returns the user's current cart, including the total price and items in the cart.")]
+        public Cart GetCart()
+        {
+            return _Cart;
+        }
+    }
+
+    public class Cart
+    {
+        public List<Pizza> pizzas { get; set; } = new List<Pizza>();
+
+    }
+
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Prcie { get; set; }
+
+    }
+
+
+    public class Menu
+    {
+        public List<Pizza> pizzas { get; set; } = new List<Pizza>();
+
+        public Menu()
+        {
+            pizzas.Add(new Pizza { Id = 1, Name = "瑪格麗特", Description = "Pepperoni, cheese, and tomato sauce", Prcie = 10 });
+            pizzas.Add(new Pizza { Id = 2, Name = "起司", Description = "Cheese and tomato sauce", Prcie = 8 });
+            pizzas.Add(new Pizza { Id = 3, Name = "夏威夷", Description = "Mushrooms, peppers, onions, and tomato sauce", Prcie = 12 });
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,57 @@
+﻿using Azure;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+namespace Test
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+            kernelBuilder.AddOpenAIChatCompletion(
+                modelId: "gpt-4o-mini",
+                apiKey: ""
+
+            );
+            kernelBuilder.Plugins.AddFromType<OrderPizzaPlugin>("OrderPizza");
+            Kernel kernel = kernelBuilder.Build();
+
+            IChatCompletionService chatCompletion = kernel.GetRequiredService<IChatCompletionService>();
+
+            OpenAIPromptExecutionSettings settings = new()
+            {
+                FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+            };
+
+            ChatHistory chatHistory = [];
+            string firstMessage = "你好，請給我菜單";
+            chatHistory.AddUserMessage(firstMessage);
+            Console.WriteLine(firstMessage);
+
+            while (true)
+            {
+                ChatMessageContent result = await chatCompletion.GetChatMessageContentAsync(chatHistory, settings, kernel);
+
+                // Check if the AI model has generated a response.
+                if (result.Content is not null)
+                {
+                    Console.WriteLine(result.Content);
+                }
+
+                // Adding AI model response containing chosen functions to chat history as it's required by the models to preserve the context.
+                chatHistory.Add(result);
+                var userInput = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(userInput))                
+                    break;
+                
+
+                chatHistory.AddUserMessage(userInput);
+            }
+
+
+        }
+    }
+}
